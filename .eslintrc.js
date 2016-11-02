040de57de608caf4353d7b7951338626096a76ff
rodoabad <rodoabad@gmail.com>
2016-11-01 19:02:14 -0500

    chore: update package dependencies and ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
-  "extends": "rodoabad",
-  "rules": {
-    "no-magic-numbers": 0,
-    "max-depth": 0
-  }
+    "extends": "rodoabad",
+    "rules": {
+        "no-magic-numbers": 0,
+        "max-depth": 0,
+        "object-curly-newline": 0
+    }
 };
